26a3796eb6bb18c678ae91184404bc77b3e0aa84
Tammy Do <tammytdo11@gmail.com>
2019-02-11 19:04:27 -0800

    question 2 passing

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 'use strict';
 /////////////////////////////////////
 
-//TESTING GH WORKFLOW
-//TESTING GH WORKFLOW
-//TESTING GH WORKFLOW
-
-
 /* Problem 1 (this is your demo that we'll solve in class)
 Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
 
@@ -13,17 +8,19 @@ Write a function called sum() that takes in two numbers as arguments and then re
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
-// Write your code here
+
+// Write your code here. LINE COMPLETE.
 function sum(a, b) { //eslint-disable-line
   var resultSum = a + b;
   var resultString = 'The sum of '+ a + ' and ' + b + ' is ' + resultSum + '.';
   return [resultSum, resultString];
 }
 
-// Here is the test for sum(); uncomment it to run it
+// Here is the test for sum(); uncomment it tdo run it. LINE COMPLETE.
 testSum(4, 7);
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question! 
+//ACP = Add, commit, push
 
 /////////////////////////////////////
 /* Problem 2
@@ -35,10 +32,11 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  var resultProduct = a * b;
+  var resultString = 'The product of ' + a + ' and ' + 9 +' is ' + resultProduct + '.'
 }
 
-// Here is the test for multiply(); uncomment it to run it
+//Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
