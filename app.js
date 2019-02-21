af7a5594a16510cd78b8a5e246e2065ca8fb32ea
tammytdo <37233625+tammytdo@users.noreply.github.com>
2019-02-20 23:49:09 -0800

    update question 5 passing

--- app.js
+++ app.js
@@ -128,7 +128,7 @@ Test this function by hand in the console to get it working, and when you think 
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
